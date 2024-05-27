417efe9d30eef5c3eceff420d1b66e46804c38c8
AberQ <egor.master2017@gmail.com>
2024-05-27 22:13:49 +0500

    First commit

---
+++ sitewomen/women/templatetags/women_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.db.models import Count
+
+import women.views as views
+from women.models import Category, TagPost
+
+register = template.Library()
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(cat_selected=0):
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
+    return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
---
+++ sitewomen/women/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, "year4")
+
+urlpatterns = [
+    path('', views.index, name='home'),  # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<slug:post_slug>/', views.show_post, name='post'),
+    path('category/<slug:cat_slug>/', views.show_category, name='category'),
+    path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),
+]
